234ab78c94d47105b7e83c10a77f4702e19ebccf
KLEBER DUARTE <kduarte@BRAZIL.SIAE>
2022-04-11 09:24:09 -0300

    Atualização do Visual da  Página Inicial e da Barra de Navegação

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
-db = SQLAlchemy(app)
+db = SQLAlchemy(application)
 
 
 class User(db.Model):
@@ -26,50 +26,50 @@ class Equipment(db.Model):
     sbb = db.Column(db.String(50))
 
 
-@app.route('/')
+@application.route('/')
 def index():
-    return render_template('index.html', titulo='Relação de Equipamentos')
+    return render_template('index.html', titulo='Página Inicial')
 
-@app.route('/idu')
+@application.route('/idu')
 def idu():
     table = 'idu'
     lista = Equipment.query.filter_by(family='IDU').all()
     return render_template('lista_filter.html', titulo="IDU's", lista=lista, table=table)
 
 
-@app.route('/odu')
+@application.route('/odu')
 def odu():
     table = 'odu'
     lista = Equipment.query.filter_by(family='ODU').all()
     return render_template('lista_filter.html', titulo="ODU's", lista=lista, table=table)
 
 
-@app.route('/module')
+@application.route('/module')
 def module():
     table = 'module'
     lista = Equipment.query.filter_by(family='MODULE').all()
     return render_template('lista_filter.html', titulo="AGS20L MODULE's", lista=lista, table=table)
 
 
-@app.route('/sfp')
+@application.route('/sfp')
 def sfp():
     table = 'sfp'
     lista = Equipment.query.filter_by(family='SFP').all()
     return render_template('lista_filter.html', titulo="SFP's", lista=lista, table=table)
 
-@app.route('/coupler')
+@application.route('/coupler')
 def coupler():
     table = 'coupler'
     lista = Equipment.query.filter_by(family='COUPLER').all()
     return render_template('lista_filter.html', titulo="COUPLER's", lista=lista, table=table)
 
-@app.route('/polekit')
+@application.route('/polekit')
 def polekit():
     table = 'polekit'
     lista = Equipment.query.filter_by(family='POLE KIT').all()
-    return render_template('lista_filter.html', titulo="POLE KIT's", lista=lista, table=table)@app.route('/polekit')
+    return render_template('lista_filter.html', titulo="POLE KIT's", lista=lista, table=table)
 
-@app.route('/obu')
+@application.route('/obu')
 def obu():
     table = 'obu'
     lista = Equipment.query.filter_by(family='OBU').all()
@@ -77,4 +77,4 @@ def obu():
 
 
 if __name__ == '__main__':
-    app.run(port=5004, debug=True)
+    application.run(port=5004, debug=True)
